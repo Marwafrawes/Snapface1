d1568dca895a8bc51f7b46e234f526a3513d51c8
Marwa <marwafrawes@gmail.com>
2023-10-09 20:26:34 +0200

    modification app

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { faceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
@@ -6,16 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent implements OnInit {
+@Input() faceSnap!:faceSnap; 
+
+
 title!: string;
 description!:string;
 createdDate!:Date;
 snaps!:number;
+imageUrl! : string;
+buttonText! : string;
 
 ngOnInit(): void {
     this.title = 'Marwa FRAWES';
     this.description = "La vie est belle Marwa FRAWES et oui elle appartient qu'à toi!!!";
     this.createdDate= new Date ();
     this.snaps = 6;  
-
+    this.imageUrl='https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+    this.buttonText='oh snap !'
+   
 }
+
+onSnap() {
+  if (this.buttonText === 'Oh Snap!') {
+    this.snaps++;
+    this.buttonText = 'Oops, unSnap!';
+  } else {
+    this.snaps--;
+    this.buttonText = 'Oh Snap!';
+  }
+}
+
 }
---
+++ src/app/models/face-snap.model.ts
@@ -0,0 +1,10 @@
+import { Title } from "@angular/platform-browser";
+
+export class faceSnap {
+constructor(public title:string, 
+    public description:string,
+    public imageUrl:string, 
+    public createdDate:Date, 
+    public snaps:number){
+}
+}
